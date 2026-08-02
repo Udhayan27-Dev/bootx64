264e79240a1e2477e824a37afd7da382011363b7
Udhayan K <kudhayan27@gmail.com>
2026-08-02 10:01:07 +0530

    Feat: Impl the loading of the kernel file from the disk

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use bootloader_x86_64_common::{
     Kernel, RawFrameBufferInfo, SystemInfo, legacy_memory_region::LegacyFrameAllocator,
 };
-use uefi::{CStr8, CStr16, cstr16};
+use uefi::{CStr8, CStr16, boot, cstr16, proto::media::file::{File, FileAttribute, FileInfo}};
 
 #[derive(Debug,Clone,Copy)]
 pub enum BootMode{
@@ -41,5 +41,36 @@ fn load_kernel(boot_mode: BootMode) -> Option<Kernel<'static>>{
     let kernel_slice = load_file_from_boot_method(&KERNEL_FILE, boot_mode)?;
     Some(Kernel::parse(kernel_slice))
 }
+
+fn load_file_from_boot_method(filename: &BootFile, boot_mode: BootMode) -> Option<&'static mut [u8]>{
+    match boot_mode{
+        BootMode::Disk => load_file_from_disk(filename.disk),
+        BootMode::Tftp => load_file_from_tftp_boot_server(filename.tftp),
+    }
+}
+=
+fn load_file_from_disk(filename: &CStr16) -> Option<&'static mut [u8]>{
+    let mut file_system = boot::get_image_file_system(boot::image_handle()).ok()?;
+    
+    let mut root = file_system.open_volume().unwrap();
+    let file_handle_result = root.open(filename, FileMode::Read, FileAttribute::empty());
+    let file_handle = file_handle_result.ok()?;
+
+    let mut file = match file_handle.into_type().unwrap() {
+        uefi::proto::media::file::FileType::Regular(f) => f,
+        uefi::proto::media::file::FileType::Dir(_) => panic!(),
+    };
+
+    let mut buf = [0;500];
+    let file_info: &mut FileInfo = file.get_info(&mut buf).unwrap();
+    let file_size = usize::try_from(file_info.file_size()).unwrap();\
+
+    let file_slice = allocate_loader_data(file_size);
+    file.read(file_slice).unwrap();
+
+    Some(file_slice)
+        
+}
+
 
 
