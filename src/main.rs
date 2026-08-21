fad03446846c62df672c0a7b376931e131d099b6
Udhayan K <kudhayan27@gmail.com>
2026-08-21 15:39:14 +0530

    wrote logics to the code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![no_std] //neglects std RUST
 #![no_main] //neglects std main in RUST
-#![deny(unsafe_op_in_unsafe_fn)] 
+#![deny(unsafe_op_in_unsafe_fn)]
 use core::{net::{IpAddr, Ipv4Addr}, ptr, slice, usize};
 
 use bootloader_x86_64_common::{
     Kernel, RawFrameBufferInfo, SystemInfo, legacy_memory_region::LegacyFrameAllocator,
 };
+use bootloader_boot_config::BootConfig;
 use uefi::{CStr8, CStr16, boot::{self, MemoryType, ScopedProtocol}, cstr16, proto::{ProtocolPointer, device_path::DevicePath, hii::config, loaded_image::LoadedImage, media::file::{File, FileAttribute, FileInfo}, network::pxe::{BaseCode, DhcpV4Packet}}};
 
 #[derive(Debug,Clone,Copy)]
@@ -42,25 +43,31 @@ fn main() -> Status{
         kernel = load_kernel(BootMode::Tftp);
     }
     let kernel = kernel.expect("failed to load kernel");
-
+    //loads the config file from the specific disk and write to the RAM and return the starting address of the location
     let config_file = load_config_file(boot_mode);
+    //temporary slot initialized tot store the Error if occur in the future becoz the screen in not loaded yet to display error storing the error help later t
     let mut error_loading_conifg: Option<serde_json_core::de::Error> = None;
-    let mut config:BootConfig = match config_file.as_deref().map(serde_json_core::from_slice).transpose()
-        {
+    let mut config:BootConfig = match config_file
+        //it is used to refernce the value inside the Option so the config_file remains original
+        .as_deref()
+        //this serde_json_core is the is key to convert the bytes into the json format and match it ot the BootCOnfig Struct
+        .map(serde_json_core::from_slice)
+        .transpose()
+    {
             Ok(data) => data.unwrap_or_default().0,
             Err(err) => {
                 error_loading_conifg = Some(err);
+                //if the file does not exist the frame is set to default config
                 Default::default()
-            }            
-        };
-    
+            }
+    };
 }
 
 fn load_config_file(boot_mode: BootMode) -> Option<&'static mut [u8]>{
     load_file_from_boot_method(&CONFIG_FILE, boot_mode)
 }
 
-fn load_ramdisk(boot_mode: BootMode) -> Option<&'static mut [u8]>{
+fn load_ramdisk(boot_mode: BootMode) -> Option<&'static mut [u8]>{ig_
     load_file_from_boot_method(&RAMDISK_FILE, boot_mode)
 }
 
@@ -78,7 +85,7 @@ fn load_file_from_boot_method(filename: &BootFile, boot_mode: BootMode) -> Optio
 
 fn load_file_from_disk(filename: &CStr16) -> Option<&'static mut [u8]>{
     let mut file_system = boot::get_image_file_system(boot::image_handle()).ok()?;
-    
+
     let mut root = file_system.open_volume().unwrap();
     let file_handle_result = root.open(filename, FileMode::Read, FileAttribute::empty());
     let file_handle = file_handle_result.ok()?;
@@ -96,7 +103,7 @@ fn load_file_from_disk(filename: &CStr16) -> Option<&'static mut [u8]>{
     //this writes the file into the memory
     file.read(file_slice).unwrap();
     //returns the memory address
-    Some(file_slice)        
+    Some(file_slice)
 }
 
 //In hardware memory management, RAM is not handed out byte-by-byte.
@@ -104,15 +111,15 @@ fn load_file_from_disk(filename: &CStr16) -> Option<&'static mut [u8]>{
 
 //In hardware memory management, RAM is not handed out byte-by-byte.
 //It is divided into fixed-size chunks called pages
-//4KiB(4096 bytes) is standard page size in x86_64 
+//4KiB(4096 bytes) is standard page size in x86_64
 fn allocate_loader_data(size: usize) -> &'static mut [u8] {
     //this returns the pointer to the allocated memory in RAM
     let mut ptr = boot::allocate_pages(boot::AllocateType::AnyPages, MemoryType::LOADER_DATA, ((size - 1)/ 4096) + 1,)
         .expect("Failed to allocate memory for the file");
-    
+
     //the allocated memory will be filled with garbage value,
     //so replacing it with zero to write our kernel file
-    unsafe{ptr::write_bytes(ptr.as_ptr(), 0, size)};   
+    unsafe{ptr::write_bytes(ptr.as_ptr(), 0, size)};
     unsafe {slice::from_raw_parts_mut(ptr.as_ptr(), size)}
 }
 
@@ -134,7 +141,7 @@ fn load_file_from_tftp_boot_server(name: &CStr8) -> Option<&'static mut[u8]> {
     //load kernel file
     base_code
         .tftp_read_file(&server_ip.into(), name, Some(slice))
-        .expect("Failed tot read kernel file from the TFTP boot server");    
+        .expect("Failed tot read kernel file from the TFTP boot server");
     Some(slice)
 }
 
@@ -146,15 +153,15 @@ fn open_pxe_base_code() -> Option<boot::ScopedProtocol<BaseCode>> {
     base_code.mode().dhcp_ack_received().then_some(base_code)
 }
 
-// Protocol pointer - 
-fn locate_and_open_protocol_from_image_device_path<P: ProtocolPointer + ?Sized>() 
+// Protocol pointer :
+fn locate_and_open_protocol_from_image_device_path<P: ProtocolPointer + ?Sized>()
 -> Option<boot::ScopedProtocol<P>> {
     let image_handle = boot::image_handle();//this returns the unqine ID of the image(here our rust's uefi file)
     let loaded_image = boot::open_protocol_exclusive::<LoadedImage>(image_handle).ok()?;//this gives access to the hardware we were loaded
     let device_handle = loaded_image.device()?;//this returns the Unique ID of the device where the image is booted like USB or hardrive etc
-    let device_path = boot::open_protocol_exclusive::<DevicePath>(device_handle).ok()?;//this returns the url like path of the booted device  
-   //In C, passing a pointer that gets modified requires passing a "pointer to a pointer" (DevicePath**). In Rust, the safe equivalent of a pointer to a pointer is a mutable reference to a reference (&mut &T). 
+    let device_path = boot::open_protocol_exclusive::<DevicePath>(device_handle).ok()?;//this returns the url like path of the booted device
+   //In C, passing a pointer that gets modified requires passing a "pointer to a pointer" (DevicePath**). In Rust, the safe equivalent of a pointer to a pointer is a mutable reference to a reference (&mut &T).
    //By writing &mut &*device_path, we satisfy Rust's borrow checker while allowing the UEFI firmware to safely advance the pointer in memory as it searches the hardware tree.
     let handle = boot::locate_device_path::<P>(&mut &*device_path).ok()?;
-    boot::open_protocol_exclusive::<P>(handle).ok()    
+    boot::open_protocol_exclusive::<P>(handle).ok()
 }
